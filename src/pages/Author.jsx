db1c3c960f6e97809d91347ff18b3bcbc57c8faa
Madhavankn <146508665+Madhavankn@users.noreply.github.com>
2024-11-08 13:54:17 +0530

    Added components

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -2,25 +2,29 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-const Book = ({setidbook}) => {
 
-  const [bookData,setbookData] = useState([])
+
+const Author = ({setidauthor}) => {
+
+  
+  const [authorData,setauthorData] = useState([])
   const [deleteData ,setDeleteData] = useState()
   const navigate = useNavigate()
 
   useEffect(()=>{
-   BookfetchData()
+    authorfetchData()
   },[deleteData])
 
-  const BookfetchData = async() =>{
+  const authorfetchData = async() =>{
     await axios.get('https://667036a10900b5f87249cf8f.mockapi.io/api/library')
-    .then((res)=>setbookData(res.data))
+    .then((res)=>setauthorData(res.data))
     .catch((error)=>{console.log(error)})
   }
 
   const geteditid =(id)=>{  
-    setidbook(id)
-    navigate(`/edit/${id}`)
+    setidauthor(id)
+
+    navigate(`/editauthor/${id}`)
   }
 
   const delData = async(id)=>{
@@ -29,11 +33,13 @@ const Book = ({setidbook}) => {
     .catch((error)=>{console.log(error)})
   }
 
+
+
   return (
     <>
 
  <div className='container' >
-  {bookData.map((element,index)=>{
+  {authorData.map((element,index)=>{
     return(
       <div className="col-md-12" key={index}>
       <div className="box m-3">
@@ -42,23 +48,20 @@ const Book = ({setidbook}) => {
             <tr>
               <td> <h5><strong>#</strong>{element.id}</h5></td>
             </tr>
+           
             <tr>
-              <td>  <h5><strong>Title </strong></h5></td>
-              <td><h5> : {element.title}</h5></td>
-            </tr>
-            <tr>
-              <td>  <h5><strong>Author  </strong></h5></td>
+              <td>  <h5><strong>Author Name </strong></h5></td>
               <td><h5> : {element.author}</h5></td>
             </tr>
 
             <tr>
-              <td>  <h5><strong>ISNB-Number </strong></h5></td>
-              <td><h5> : {element.ISBN_number}</h5></td>
+              <td>  <h5><strong>Data of Brith</strong></h5></td>
+              <td><h5> : {element.author_dOB}</h5></td>
             </tr>
 
             <tr>
-              <td>  <h5><strong>Publication-Data   </strong></h5></td>
-              <td><h5> : {element.publication_data}</h5></td>
+              <td>  <h5><strong>Biography</strong></h5></td>
+              <td><h5> : {element.author_biography}</h5></td>
             </tr>
 
          
@@ -86,6 +89,8 @@ const Book = ({setidbook}) => {
     </>
     
   )
+
+
 }
 
-export default Book
+export default Author
